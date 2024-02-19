8f068a5c773cc3829690a5cb3069b0f5761ec0ab
frumilly <ouerfelli.fzahra2@gmail.com>
2024-02-19 09:26:09 +0100

    affchage package

---
+++ src/lib/Modal.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Modal.css'; 
+
+/**
+ * Composant de modal personnalisée.
+ * @param {Object} props - Les props du composant.
+ * @param {boolean} props.isOpen - Indique si la modal est ouverte ou non.
+ * @param {string} [props.message="Hello modal"] - Le message à afficher dans la modal.
+ * @param {function} props.onOk - La fonction à appeler lorsque l'utilisateur clique sur le bouton "OK".
+ * @param {function} props.onClose - La fonction à appeler lorsque l'utilisateur ferme la modal.
+ * @returns {JSX.Element|null} Le composant Modal.
+ */
+const Modal = ({ isOpen, message = "Hello modal", onOk, onClose }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  /**
+   * Gère le clic sur le bouton "OK".
+   * Appelle d'abord la fonction onOk, puis onClose.
+   * @returns {void}
+   */
+  const handleOkButtonClick = () => {
+    onOk();
+    onClose();
+  };
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-container">
+        <div className="modal-content">
+          <button className="close-btn" onClick={onClose}>X</button>
+          <p>{message}</p>
+          <div className="modal-buttons">
+            <button onClick={handleOkButtonClick}>OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
